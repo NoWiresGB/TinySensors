bb0999ab5b696ed0d4a52f7165783ce67b5ae254
trizmark <zoltan.markella@gmail.com>
2020-09-28 21:07:04 +0100

    first commit

---
+++ NetworkSniffer/src/main.cpp
@@ -0,0 +1,79 @@
+#include <Arduino.h>
+#include <RFM69.h>
+#include <RFM69_ATC.h>
+
+#define NODEID      99
+#define NETWORKID   89
+#define GATEWAYID   1
+
+#define FREQUENCY     RF69_433MHZ
+#define ENCRYPTKEY    "sampleEncryptKey" //has to be same 16 characters/bytes on all nodes, not more not less!
+#define ENABLE_ATC    //comment out this line to disable AUTO TRANSMISSION CONTROL
+
+#define SERIAL_BAUD 115200
+
+#ifdef ENABLE_ATC
+  RFM69_ATC radio;
+#else
+  RFM69 radio;
+#endif
+
+void setup() {
+  Serial.begin(SERIAL_BAUD);
+  Serial.println("RFM69 sniffer starting up");
+  radio.initialize(FREQUENCY,NODEID,NETWORKID);
+#ifdef IS_RFM69HW_HCW
+  radio.setHighPower(); //must include this only for RFM69HW/HCW!
+#endif
+  radio.encrypt(ENCRYPTKEY);
+  char buff[50];
+  sprintf(buff, "\nTransmitting at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
+  Serial.println(buff);
+
+  // enable spy mode straight away
+  radio.spyMode(true);
+}
+
+char hexDigit(byte v)
+{
+  v &= 0x0F; // just the lower 4 bits
+
+  return v < 10 ? '0' + v : 'A' + (v - 10);
+}
+
+void loop() {
+  //process any serial input
+  if (Serial.available() > 0)
+  {
+    char input = Serial.read();
+    if (input == 'r') //d=dump register values
+      radio.readAllRegs();
+  }
+
+  //check for any received packets
+  if (radio.receiveDone())
+  {
+    Serial.print('[');
+    Serial.print(radio.SENDERID, DEC);
+    Serial.print("] [");
+
+    // hex dump of payload
+    // max data length is 61, so we allocate 2x61 + 1 for string termination
+    char  hexData[123];
+    byte  ptr = 0;
+    for (byte i = 0; i < radio.DATALEN; i++){
+      hexData[ptr++] = hexDigit(radio.DATA[i] >> 4);
+      hexData[ptr++] = hexDigit(radio.DATA[i]);
+    }
+    hexData[ptr] = '\0';
+    String hexPayload = String(hexData);
+    Serial.print(hexData);
+    Serial.print("] ");
+
+    Serial.print("   [RX_RSSI:");
+    Serial.print(radio.readRSSI());
+    Serial.print("]");
+
+    Serial.println();
+  }
+}
