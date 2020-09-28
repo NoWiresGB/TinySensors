47489494fdfa4925ff7c50b4a8a9c04e9822dfaf
trizmark <zoltan.markella@gmail.com>
2020-09-28 21:12:01 +0100

    Minor changes to the network sniffer

--- NetworkSniffer/src/main.cpp
+++ NetworkSniffer/src/main.cpp
@@ -7,8 +7,8 @@
 #define GATEWAYID   1
 
 #define FREQUENCY     RF69_433MHZ
-#define ENCRYPTKEY    "sampleEncryptKey" //has to be same 16 characters/bytes on all nodes, not more not less!
-#define ENABLE_ATC    //comment out this line to disable AUTO TRANSMISSION CONTROL
+#define ENCRYPTKEY    "sampleEncryptKey" // 16 character key for the network
+#define ENABLE_ATC    // enable auto-transmission control
 
 #define SERIAL_BAUD 115200
 
@@ -22,9 +22,7 @@ void setup() {
   Serial.begin(SERIAL_BAUD);
   Serial.println("RFM69 sniffer starting up");
   radio.initialize(FREQUENCY,NODEID,NETWORKID);
-#ifdef IS_RFM69HW_HCW
-  radio.setHighPower(); //must include this only for RFM69HW/HCW!
-#endif
+
   radio.encrypt(ENCRYPTKEY);
   char buff[50];
   sprintf(buff, "\nTransmitting at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
@@ -34,25 +32,23 @@ void setup() {
   radio.spyMode(true);
 }
 
-char hexDigit(byte v)
-{
+char hexDigit(byte v) {
   v &= 0x0F; // just the lower 4 bits
 
   return v < 10 ? '0' + v : 'A' + (v - 10);
 }
 
 void loop() {
-  //process any serial input
-  if (Serial.available() > 0)
-  {
+  // process any serial input
+  if (Serial.available() > 0) {
     char input = Serial.read();
-    if (input == 'r') //d=dump register values
+    if (input == 'r') {
       radio.readAllRegs();
+    }
   }
 
   //check for any received packets
-  if (radio.receiveDone())
-  {
+  if (radio.receiveDone()) {
     Serial.print('[');
     Serial.print(radio.SENDERID, DEC);
     Serial.print("] [");
@@ -61,7 +57,7 @@ void loop() {
     // max data length is 61, so we allocate 2x61 + 1 for string termination
     char  hexData[123];
     byte  ptr = 0;
-    for (byte i = 0; i < radio.DATALEN; i++){
+    for (byte i = 0; i < radio.DATALEN; i++) {
       hexData[ptr++] = hexDigit(radio.DATA[i] >> 4);
       hexData[ptr++] = hexDigit(radio.DATA[i]);
     }
